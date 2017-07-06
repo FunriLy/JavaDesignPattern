f94030880f41dfd80e00a6257baa9279493bfff9
FunriLy <fr479910770@qq.com>
2017-07-06 14:04:13 +0800

    project init

---
+++ src/main/java/com/qg/fangrui/JavaDesignPattern/JavaDesignPatternApplication.java
@@ -0,0 +1,12 @@
+package com.qg.fangrui.JavaDesignPattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDesignPatternApplication.class, args);
+	}
+}
