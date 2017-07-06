2860493a7c128db947564f74c4c675476532ef08
FunriLy <fr479910770@qq.com>
2017-07-06 21:17:32 +0800

    适配器模式

--- src/main/java/com/qg/fangrui/JavaDesignPattern/Adapter/Adapter.java
+++ src/main/java/com/qg/fangrui/JavaDesignPattern/Adapter/Adapter.java
@@ -1,8 +1,14 @@
 package com.qg.fangrui.JavaDesignPattern.Adapter;
 
 /**
+ * 适配器模式
  * Created by FunriLy on 2017/7/6.
  * From small beginnings comes great things.
  */
 public class Adapter {
+    static {
+        System.out.println("适配器模式分为两种");
+        System.out.println("1、一种是采用继承的适配器模式");
+        System.out.println("2、一种是采用委托的适配器模式");
+    }
 }
